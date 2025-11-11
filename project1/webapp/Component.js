b10e23cecf84921bcffbb8d0aefee5af97d9fc78
ilana <ilanalipkina@gmail.com>
2025-11-11 15:35:21 +0400

    refactor: move booksModel to models

--- project1/webapp/Component.js
+++ project1/webapp/Component.js
@@ -1,10 +1,6 @@
 sap.ui.define(
-  [
-    "sap/ui/core/UIComponent",
-    "project1/model/models",
-    "sap/ui/model/json/JSONModel",
-  ],
-  (UIComponent, models, JSONModel) => {
+  ["sap/ui/core/UIComponent", "project1/model/models"],
+  (UIComponent, models) => {
     "use strict";
 
     return UIComponent.extend("project1.Component", {
@@ -14,61 +10,11 @@ sap.ui.define(
       },
 
       init() {
-        // call the base component's init function
         UIComponent.prototype.init.apply(this, arguments);
 
-        // set the device model
         this.setModel(models.createDeviceModel(), "device");
+        this.setModel(models.createBooksModel(), "booksModel");
 
-        const aBooks = {
-          books: [
-            {
-              id: "book-001",
-              name: "The Count of Monte Cristo",
-              author: "Alexandre Dumas",
-              genre: "Historical novel / Adventure",
-              releaseDate: "1846",
-              availableQuantity: 4,
-            },
-            {
-              id: "book-002",
-              name: "To Kill a Mockingbird",
-              author: "Harper Lee",
-              genre: "Southern Gothic / Bildungsroman",
-              releaseDate: "1960",
-              availableQuantity: 7,
-            },
-            {
-              id: "book-003",
-              name: "Madame Bovary",
-              author: "Gustave Flaubert",
-              genre: "Realist novel",
-              releaseDate: "1857",
-              availableQuantity: 3,
-            },
-            {
-              id: "book-004",
-              name: "The Thursday Murder Club",
-              author: "Richard Osman",
-              genre: "Crime / Mystery",
-              releaseDate: "2020",
-              availableQuantity: 5,
-            },
-            {
-              id: "book-005",
-              name: "Cujo",
-              author: "Stephen King",
-              genre: "Horror",
-              releaseDate: "1981",
-              availableQuantity: 2,
-            },
-          ],
-        };
-
-        const oModel = new JSONModel(aBooks);
-        this.setModel(oModel);
-
-        // enable routing
         this.getRouter().initialize();
       },
     });
