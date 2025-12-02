dbaeb9f8f103e5eab3875ddedb506da1d74ec36a
ilanalis <112199994+ilanalis@users.noreply.github.com>
2025-12-02 08:36:48 +0400

    feat: implement tab routing (#15)

--- project1/webapp/controller/Main.controller.js
+++ project1/webapp/controller/Main.controller.js
@@ -1,5 +1,26 @@
 sap.ui.define(["project1/controller/BaseController"], (BaseController) => {
   "use strict";
 
-  return BaseController.extend("project1.controller.Main", {});
+  return BaseController.extend("project1.controller.Main", {
+    onInit() {
+      const oRouter = this.getOwnerComponent().getRouter();
+
+      oRouter.getRoute("RouteMain").attachPatternMatched(() => {
+        oRouter.navTo("tab", { tabName: "JSONModel" }, true);
+      });
+      oRouter.getRoute("tab").attachPatternMatched(this._onTabMatched, this);
+    },
+
+    _onTabMatched(oEvent) {
+      const tabName = oEvent.getParameter("arguments").tabName;
+
+      const oIconTabBar = this.byId("mainIconTabBar");
+      oIconTabBar.setSelectedKey(tabName);
+    },
+
+    onFilterSelect(oEvent) {
+      const key = oEvent.getParameter("key");
+      this.getOwnerComponent().getRouter().navTo("tab", { tabName: key });
+    },
+  });
 });
